53767435a14cdf2c9148f93e9447a827863c48d9
Keir Staple and Oskar Radon <keirstaple@comcast.net>
2016-03-29 13:59:59 -0700

    add delete functionality to admin page

--- app/components/admin-article-tile.js
+++ app/components/admin-article-tile.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  
+  actions: {
+    delete(article) {
+      if (confirm('Are you sure you want to delete this article?')) {
+        this.sendAction('destroyArticle', article);
+      }
+    }
+  }
 });
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('article');
+  },
+
+  actions: {
+    destroyArticle(article) {
+      article.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
